9297872592d0cfc65caa70e0cf8c56e8ffcb53fd
medyll <43032494+medyll@users.noreply.github.com>
2022-02-28 09:11:44 +0100

    more

---
+++ src/lib/vendor/alert/AlertDemo.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Alert from '$lib/vendor/alert/Alert.svelte';
+</script>
+
+<Alert level="success" message="Message as props" />
+
+<Alert level="info"
+	>Simple alert with button
+	<button slot="topButtonSlot">button</button>
+</Alert>
+<Alert
+	level="error"
+	on:alert:closed={()=>{alert('closed')}}  
+	>Simple alerte with message
+	<button data-close slot="buttonZoneSlot">button close</button>
+	<div slot="messageSlot">message here</div>
+</Alert>
+<Alert level="warning" message="message here">Simple alert</Alert>
+<Alert level="alert" message="message here">Simple <br />alert</Alert>
+<Alert level="discrete" message="message here">Simple</Alert>
+<Alert message="message here">
+	alert red
+	<div slot="messageSlot">description here here</div>
+	<button data-close slot="buttonZoneSlot">buttddd on</button>
+</Alert>
---
+++ src/lib/vendor/tabs/Tabs.demo.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Tabs from './Tabs.svelte';
+  import TabsItemsProps from '$lib/vendor/tabs/Tabs.svelte';
+  import Cartouche from '$lib/vendor/cartouche/Cartouche.svelte';
+
+  const items: TabsItemsProps = [
+    {label: 'theTitle', code: 'theTitle1'},
+    {label: 'theTitle bis', code: 'theTitle2'},
+    {label: 'theTitle ter', code: 'theTitle3', component: Cartouche, componentProps: {label: 'dynamic'}},
+  ];
+</script>
+<Tabs {items}>
+    tabs
+</Tabs>
