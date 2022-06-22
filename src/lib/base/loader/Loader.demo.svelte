ad73b4f3e3ab540de6981d355c654d5da954ff27
medyll <43032494+medyll@users.noreply.github.com>
2022-06-22 18:38:48 +0200

    feat: loader demo

---
+++ src/lib/base/loader/Loader.demo.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Column from '../columner/Column.svelte';
+	import Columner from '../columner/Columner.svelte';
+	import Loader from './Loader.svelte';
+</script>
+
+<div class="flex-v gap-large pad-6 pos-rel border">
+	<Columner style="height:750px">
+		<Column>
+			<Loader isLoading={true} /> 
+		</Column>
+		<Column>
+			<Loader isError={true} /></Column>
+		<Column><Loader isEmpty={true} /></Column>
+	</Columner>
+	<!-- <div>isLoading</div>
+	<div class="w-medium h-medium pos-rel border pad">
+		<Loader isLoading={true} />
+	</div>
+    <div>isError</div>
+	<div class="w-medium h-medium pos-rel">
+		<Loader isError={true} />
+	</div>
+    <div>isEmpty</div>
+	<div class="w-medium h-medium pos-rel">
+		<Loader isEmpty={true} />
+	</div> -->
+</div>
