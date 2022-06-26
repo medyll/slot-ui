491a1140b670860f3c04113b630554b6fe39a624
medyll <43032494+medyll@users.noreply.github.com>
2022-06-26 18:59:21 +0200

    fix: missing git files

---
+++ src/lib/base/divider/Divider.demo.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { ElementProps } from 'src/types';
+	import Divider from './Divider.svelte';
+	type OptType = {
+		density: ElementProps['density'][];
+		expansion: ElementProps['expansion'][];
+		direction: ['vertical', 'horizontal'];
+		shadow: [false, true];
+	};
+	let attrs = {};
+
+	// density
+	// shadow
+	// extension
+	// direction
+	const options: OptType & Record<string, any> = {
+		density: ['none', 'tight', 'default', 'medium', 'kind'],
+		expansion: ['full', 'padded', 'centered'],
+		direction: ['vertical', 'horizontal'],
+		shadow: [false, true]
+	};
+
+	function changeAttr(attr: any) {
+		attrs = { ...attrs, ...attr };
+	}
+</script>
+
+<div class="flex-v gap-large">
+	<h5>{'<Divider />'}</h5>
+	<cite
+		><p>
+			What is separating before from after is not a time nor a place : it's a component <br /> B. Franklin,
+			1854
+		</p></cite
+	>
+	<div class=" flex-v gap-small">
+		{#each Object.keys(options) as option}
+			<h5 class="pad">{option}</h5>
+			<div class=" flex-h flex-wrap gap-small">
+				{#each options[option] as optionVal}
+					{@const inlineObj = { [option]: optionVal }}
+					<div class="w-large pos-rel  text-center">
+						<div class="pad">{option} : {optionVal}</div>
+						<div class="position:relative;" class:flex-h={optionVal === 'vertical'}  class:flex-align-middle={optionVal === 'vertical'}>
+							<div> 
+								What is before<br />
+                                in fact
+							</div>
+							<Divider {...inlineObj} />
+							<div>could be after <br /> also ?</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
+</div>
