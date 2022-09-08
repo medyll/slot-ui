296d9b4e9d6e108a3625abf1c20379abf5616cef
medyll <43032494+medyll@users.noreply.github.com>
2022-09-08 20:18:49 +0200

    ver: .68

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type TIcon = string;
+
+export namespace Drawer {
+  export let title: string;
+}
+
+export interface ElementProps {
+  sizeType: 'tiny' | 'small' | 'medium' | 'default' | 'large' | 'big' | 'full' | 'auto',
+  inputHeight: 'tiny' | 'small' | 'default' | 'large' | 'none',
+  density: 'none' | 'tight' | 'default' | 'medium' | 'kind'
+  expansion: 'full' | 'padded' | 'centered',
+  alignment: 'center' | 'left' | 'right',
+  data: Record<string, any>;
+  action: (event, data: ElementProps['data']) => void;
+  icon: | TIcon
+  | { type: 'fa', name: TIcon };
+}
+
+export type Data = Record<string, any>
