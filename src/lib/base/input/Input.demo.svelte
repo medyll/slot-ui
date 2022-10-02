2da7e917d04ad44aecbfc01ef04da599d3be3da4
medyll <43032494+medyll@users.noreply.github.com>
2022-10-02 03:55:23 +0200

    chore: input  demo

--- src/lib/base/input/Input.demo.svelte
+++ src/lib/base/input/Input.demo.svelte
@@ -1,45 +1,88 @@
 <script lang="ts">
-	import type { ElementProps } from '$typings/index.js';
+	/* demo */
+	import ComponentDemo from '$_components/ComponentExample.svelte';
+	import Demoer from '$lib/base/demoer/Demoer.svelte';
+	import DemoPage from '$lib/base/demoer/DemoPage.svelte';
+	import { defaultsArgsFromProps } from '../demoer/demoer.utils.js';
+	import Icon from '../icon/Icon.svelte';
+	/* demo */
 
 	import Input from '$lib/base/input/Input.svelte';
+	import type { ElementProps } from '$typings/index.js';
+
 	type ParameterType = {
 		sizes: ElementProps['sizeType'][];
 		inputHeight: ElementProps['inputHeight'][];
+	}; 
+
+	let parametersSlot: any = {
+		size: {
+			type: 'size-preset',
+			values: ['medium', 'tiny', 'small', 'large']
+		},
 	};
 
-	let parameters: ParameterType = {
-		sizes: ['tiny', 'small', 'medium', 'large'],
-		inputHeight: ['tiny', 'small', 'old', 'large']
+	let componentArgsSlot = {
+		value: 'input value',
+		size: defaultsArgsFromProps('size', parametersSlot), 
 	};
+
+	let parametersProps: any = {
+		icon: {
+			type: 'icon',
+			values: ['search', undefined]
+		},
+		endIcon: {
+			type: 'endIcon',
+			values: ['search', undefined]
+		},
+		height: {
+			type: 'size-preset',
+			values: ['small','tiny', 'small',  'large']
+		},
+		...parametersSlot, 
+	};
+
+	let componentArgsProps = {
+		...componentArgsSlot
+	};
+
+	let codeSlot = `<Input >
+							<Icon slot="startInputSlot" icon="close" />
+							<Icon slot="endInputSlot" icon="close" />
+						</Input>`;
+	let codeSlotProps = `<Input icon={icon} endIcon={icon} />`;
 </script>
 
-<div class="flex-v gap-large">
-	<h5>sized with inputs</h5>
-	<div class="flex-h flex-wrap gap-small">
-		{#each parameters.sizes as size}
-			<div class="flex-v gap-small">
-				<div>{size}</div>
-				<div><Input value={size} {size}>{size}</Input></div>
-			</div>
-		{/each}
+<ComponentDemo component="Input">
+	<div class="flex-v gap-large">
+		<DemoPage title="Using slots" component="Input" code={codeSlot}>
+			<Demoer parameters={parametersSlot} componentArgs={componentArgsSlot} let:activeParams>
+				<div class="pad flex-h flex-align-middle flex-wrap">
+					<div class="pad-1">
+						<Input {...activeParams}>
+							<Icon slot="startInputSlot" icon="close" /> 
+						</Input>
+					</div>
+					<div class="pad-1">
+						<Input {...activeParams} /> 
+					</div>
+					<div class="pad-1">
+						<Input {...activeParams}>
+							<Icon slot="endInputSlot" icon="close" />
+						</Input>
+					</div>
+				</div>
+			</Demoer>
+		</DemoPage>
+		<DemoPage title="Using props" component="Input" code={codeSlotProps}>
+			<Demoer parameters={parametersProps} componentArgs={componentArgsProps} let:activeParams>
+				<div class="pad flex-h">
+					<div class="pad-1">
+						<Input {...activeParams} />
+					</div>
+				</div>
+			</Demoer>
+		</DemoPage>
 	</div>
-	<h5>sized height inputs</h5>
-	<div class="flex-h flex-wrap gap-small">
-		{#each parameters.inputHeight as height}
-			<div class="flex-v gap-small">
-				<div>height {height}</div>
-				<div><Input icon="search" value="value" {height}>...</Input></div>
-			</div>
-		{/each}
-	</div>
-	<h5>start icon or slot</h5>
-	<div class="flex-h flex-wrap gap-small">
-		<div><Input icon="user" value="value">...</Input></div>
-		<div><Input icon="search" value="value">...</Input></div>
-	</div>
-	<h5>end icon or slot</h5>
-	<div class="flex-h flex-wrap gap-small">
-		<div><Input endIcon="user" value="value">...</Input></div>
-		<div><Input endIcon="search" value="value" /></div>
-	</div>
-</div>
+</ComponentDemo>
