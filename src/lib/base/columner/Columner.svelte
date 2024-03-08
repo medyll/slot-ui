a8d9f1cd4e654666431b3aec3c9194a2e613d242
medyll <43032494+medyll@users.noreply.github.com>
2024-03-08 22:30:38 +0100

    feat: Columner  migrated to svelte 5 snippets

--- src/lib/base/columner/Columner.svelte
+++ src/lib/base/columner/Columner.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
 	import type { ColumnerStoreType } from './types.js';
-	import { setContext } from 'svelte';
+	import { setContext, type Snippet } from 'svelte';
 	import { writable } from 'svelte/store';
 
 	const columner = writable<ColumnerStoreType>({});
 	setContext('columner', columner);
+	type Props = {
+		children: Snippet;
+	};
+	let { children, ...restProps } = $props<Props>();
 </script>
 
-<div class="columner"  {...$$restProps}>
-	<slot />
-	<!-- <div class="flex-main">
-		<pre>{JSON.stringify($columner, null, ' ')}</pre>
-	</div> -->
+<div class="columner" {...restProps}>
+	{#if children}
+		{@render children()}
+	{/if}
 </div>
 
 <style lang="scss">
-	@import "../../styles/slotui-vars.scss";
-	@import "../../styles/presets.scss";
+	@import '../../styles/slotui-vars.scss';
+	@import '../../styles/presets.scss';
 	.columner {
 		display: flex;
-		height: 100%; 
+		height: 100%;
 		position: relative;
 	}
 </style>
