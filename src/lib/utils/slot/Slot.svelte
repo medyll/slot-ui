1c8be32aa8e469c6c8eb63dbc02db97e9d60b419
medyll <43032494+medyll@users.noreply.github.com>
2024-03-09 02:07:42 +0100

    feat: new component slot

---
+++ src/lib/utils/slot/Slot.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	type Props = {
+		slot?: Snippet<[typeof Props['slotArgs'] ]>;
+		slotArgs: any;
+		children?: Snippet;
+	};
+
+	let { slot, children, slotArgs } = $props<Props>();
+</script>
+
+{#if slot}
+	{@render slot(slotArgs)}
+{:else if children}
+	{@render children(slotArgs)}
+{/if}
